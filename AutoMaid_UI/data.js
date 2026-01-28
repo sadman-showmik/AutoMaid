f830a02d1c5145ef886dce0c780f6432f25667df
sadman-showmik <sadman.showmik@metropolia.fi>
2026-01-28 05:30:14 +0200

    Add AutoMaid_UI folder to repository

---
+++ AutoMaid_UI/data.js
@@ -0,0 +1,39 @@
+const automaidData = {
+  summary: {
+    home_status_percent: 84,
+    automation_efficiency_percent: 79,
+    user_satisfaction_percent: 92,
+    eco_score_percent: 71
+  },
+  cleanliness: {
+    current_percent: {
+      kitchen: 42,
+      living_room: 78,
+      bedroom: 91,
+      bathroom: 55
+    },
+    last_month_avg_percent: {
+      kitchen: 61,
+      living_room: 70,
+      bedroom: 85,
+      bathroom: 58
+    }
+  },
+  wellbeing: {
+    overall_happiness_percent: {
+      current: 82,
+      last_month: 76
+    },
+    feedback_score_percent: 88
+  },
+  environment: {
+    air_quality_percent: 73,
+    water_saved_percent: 64,
+    energy_efficiency_percent: 69
+  },
+  devices_health_percent: {
+    vacuum_robot: 91,
+    air_purifier: 87,
+    washing_unit: 78
+  }
+};
